edee075b58b6fadf5a2f88cc762ecd9d6efcf83f
Michael Roth <michi_roth@gmx.ch>
2020-12-12 23:08:38 +0100

    WIP

--- MovieMoverCore/Pages/Subtitles.cshtml.cs
+++ MovieMoverCore/Pages/Subtitles.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using MovieMoverCore.Models.DTO;
 using MovieMoverCore.Services;
 
@@ -14,15 +16,26 @@ namespace MovieMoverCore.Pages
         public List<DisplaySeries> Series { get; set; }
 
         private IDatabase _database;
+        private ILogger<SubtitlesModel> _logger;
 
-        public SubtitlesModel(IDatabase database)
+        public SubtitlesModel(IDatabase database, ILogger<SubtitlesModel> logger)
         {
             _database = database;
+            _logger = logger;
         }
         
         public void OnGet()
         {
             Series = _database.GetSeries(s => !s.IsFinished).Select(s => new DisplaySeries { Id = s.Id, Name = s.Name }).OrderBy(s => s.Name).ToList();
+        }
+
+        public void OnPost(List<IFormFile> files)
+        {
+            _logger.LogDebug($"Received {files.Count} files");
+            foreach (var f in files)
+            {
+                _logger.LogDebug($"File '{f.Name}' ({f.FileName})");
+            }
         }
     }
 }
